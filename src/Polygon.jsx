67a495fc7e8fa697f0e57bf7ff46362e6366e6cf
Chun-Lin <wulin40063@gmail.com>
2020-11-27 17:04:11 +0800

    feat: 🎸 add vertex dragging features

--- src/Polygon.jsx
+++ src/Polygon.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useState, useRef, useEffect, useMemo } from 'react'
 import * as d3 from 'd3'
 
 const Polygon = () => {
-  const [coords, setCoords] = React.useState([])
-  const [pathD, setPathD] = React.useState('')
+  const [coords, setCoords] = useState([])
+  const [pathD, setPathD] = useState('')
 
-  const hull = React.useMemo(
-    () => (coords.length >= 3 ? [...d3.polygonHull(coords)] : coords),
-    [coords],
-  )
-  console.log('LOG: Polygon -> hull', hull)
+  const svgRef = useRef(null)
+  const indexRef = useRef(null)
 
-  React.useEffect(() => {
+  const vertexHandleMouseMove = event => {
+    let newPosX = event.offsetX
+    let newPosY = event.offsetY
+
+    const newCoords = [...coords]
+
+    newCoords[indexRef.current] = [newPosX, newPosY]
+    setCoords(newCoords)
+  }
+
+  const vertexHandleMouseUp = () => {
+    document.removeEventListener('mouseup', vertexHandleMouseUp)
+    document.removeEventListener('mousemove', vertexHandleMouseMove)
+  }
+
+  const vertexHandleMouseDown = () => {
+    document.addEventListener('mouseup', vertexHandleMouseUp)
+    document.addEventListener('mousemove', vertexHandleMouseMove)
+  }
+
+  useEffect(() => {
     const path = d3.path()
     coords.length !== 0 &&
-      hull.forEach(([x, y], index) => {
+      coords.forEach(([x, y], index) => {
         index === 0 ? path.moveTo(x, y) : path.lineTo(x, y)
       })
     path.closePath()
     setPathD(path)
-  }, [coords, hull, setPathD])
+  }, [coords, setPathD])
 
   return (
     <svg
@@ -30,11 +47,22 @@ const Polygon = () => {
         coords.length < 4 &&
         setCoords([...coords, [e.nativeEvent.offsetX, e.nativeEvent.offsetY]])
       }
+      ref={svgRef}
     >
-      {coords.map(([x, y], i) => (
-        <circle key={i} cx={x} cy={y} r="5" fill="green" />
-      ))}
       <path d={pathD} fill="red" stroke="black" opacity="0.5" />
+      {coords.map(([x, y], index) => (
+        <circle
+          key={index}
+          cx={x}
+          cy={y}
+          r="15"
+          fill="green"
+          onMouseDown={e => {
+            vertexHandleMouseDown(e)
+            indexRef.current = index
+          }}
+        />
+      ))}
     </svg>
   )
 }
